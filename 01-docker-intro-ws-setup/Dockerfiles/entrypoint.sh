0eb3d968a4724650fbcae79989b8e4075c046649
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-09-17 05:11:04 +0800

    add lab01

---
+++ 01-docker-intro-ws-setup/Dockerfiles/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Setup the style of color
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+echo -e "${COLOR_GREEN}Enter to docker container successfully.${COLOR_NC}"
