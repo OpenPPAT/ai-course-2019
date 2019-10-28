ccd8f398463a5e50d314580151321a5bbf37f240
Jack Chang <iamjack356@gmail.com>
2019-10-28 14:03:57 +0800

    Update run_docker.sh

--- 05-jupyter-Fashion-Mnist-dataset-pytorch/run_docker.sh
+++ 05-jupyter-Fashion-Mnist-dataset-pytorch/run_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run -it --rm --net=host \
-            --name=ai-course-lite \
             -v ${HOME}/ai-course-2019:/home/student/ai-course-2019 \
             -w /home/student/ai-course-2019/05-jupyter-Fashion-Mnist-dataset-pytorch \
             argnctu/ai-course:jupyter-05
