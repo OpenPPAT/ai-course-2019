2d938ee3a3883f53d31fec01df2ea95d3868e873
biomotion <bensonpuppy1997@gmail.com>
2019-11-02 02:53:38 +0800

    change launched docker image

--- 06-jupyter-cnn/docker_run_laptop.sh
+++ 06-jupyter-cnn/docker_run_laptop.sh
@@ -3,4 +3,4 @@ docker run -it --rm --net=host \
             --name=ai-course-lite \
             -v ${HOME}/ai-course-2019:/home/student/ai-course-2019 \
             -w /home/student/ai-course-2019/06-jupyter-cnn \
-            argnctu/ai-course:ros-jupyter-lite
+            argnctu/ai-course:jupyter-05
