596559d9f91178e1fb0122223257603ead04f3d6
biomotion <bensonpuppy1997@gmail.com>
2019-11-02 17:22:52 +0800

    update docker run

--- 06-jupyter-cnn/docker_run.sh
+++ 06-jupyter-cnn/docker_run.sh
@@ -9,7 +9,6 @@ fi
 
 
 $runtime run -it --rm --net=host \
-            --name=ai-course-lite \
             -v ${HOME}/ai-course-2019:/home/student/ai-course-2019 \
             -w /home/student/ai-course-2019/06-jupyter-cnn \
             argnctu/ai-course:jupyter-05
